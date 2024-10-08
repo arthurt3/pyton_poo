07122ab250ab45b2942cb06ea70a191ad31bc509
Victor Arthur <arthurtelesfreitas2006@gmail.com>
2024-10-08 09:00:21 -0300

    Adicionando atividades, e aulas sobre herança parte 2 no dia 08/10

---
+++ Aula6-heranca-2/principal.py
@@ -0,0 +1,13 @@
+from pessoa import Pessoa, Aluno, Professor
+
+pessoa1= Pessoa ("Getulio", 65)
+aluno1 = Aluno("Keila", 16, "2º ano do ensino medio ")
+prof1 = Professor("Augusto", 49 , "Geografia")
+
+pessoa1.info()
+
+aluno1.info()
+aluno1.estudar()
+
+prof1.info()
+prof1.ensinar()
