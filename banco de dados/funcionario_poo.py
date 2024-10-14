75ae32295f2c7992677b28eafaa01d3ec77559b1
Victor Arthur <arthurtelesfreitas2006@gmail.com>
2024-10-14 11:33:00 -0300

    Adicionando aula sobr implementação de sqlite3 em p.o.o  no dia 14/10

--- banco de dados/funcionario_poo.py
+++ banco de dados/funcionario_poo.py
@@ -18,3 +18,40 @@ class Funcionario:
         consulta.execute(tabela)
 
         return conexao
+    
+    def inserir(self , nome, salario, cargo):
+        conexao =  self.conexao()#Estamos chamando método 'conexão' que irá conectar com o banco
+        sql = "INSERT INTO funcionario VALUES(?,?,?,?)"
+
+        campos =  (None, nome, salario, cargo)
+
+        consulta = conexao.cursor()
+        consulta.execute(sql, campos)
+
+        conexao.commit()
+        print(consulta.rowcount,  "Registro inserido com sucesso!\n")
+        conexao.close()
+
+    def consultar (self):
+        conexao = self.conexao()
+        consulta = conexao.cursor()
+        sql = "SELECT * FROM funcionario"
+        consulta.execute(sql)
+        resultado = consulta.fetchall()#pegando  todos os dados da tabela
+
+        for item  in resultado:
+            print(f"Codigo: {item[0]}")
+            print(f"Nome: {item[1]}")
+            print(f"Salário: {item[2]}")
+            print(f"Cargo: {item[3]}")
+
+        conexao.close()
+
+
+
+
+        
+
+
+
+
